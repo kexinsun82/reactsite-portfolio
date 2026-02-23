bbbc941e3c5b7edd51d2d995b86e371d501574f5
Kexin Sun <kexin.sun82@gmail.com>
2026-02-23 17:18:47 -0500

    Update structure

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5174,
+    proxy: {
+      '/api': {
+        target: 'https://react-portfolioadmin-production.up.railway.app/',
+        changeOrigin: true,
+      }
+    }
+  }
 })
